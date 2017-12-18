5dad73a87bf9e513029086e94b5281dd21a59edd
moteroeiras <motero@insideone.com.ar>
2017-12-18 15:31:38 -0300

    err

--- src/Cards/methods.js
+++ src/Cards/methods.js
@@ -7,7 +7,7 @@ exports.pay = function(req, res) {
         res.status(200).send(data)
 	}, function({ code, data }) {
         console.log('==================ER==================');
-        console.log({ code, data });
+        console.log({ code : code || 400, data: data || 'No se pudo procesar el pago'});
         console.log('====================================');
         // res.status(501).send(data)
         res.send({ code, data })
